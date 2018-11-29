4e7d5c89395aee88c4d76caeca38b27d59eb9ed7
hariprasath <hariprasath.sivanandam@gmail.com>
2018-11-29 02:03:10 -0500

    init commit

---
+++ src/main/java/edu/northeastern/cs5610/WebservicesF18Application.java
@@ -0,0 +1,12 @@
+package edu.northeastern.cs5610;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebservicesF18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebservicesF18Application.class, args);
+	}
+}
