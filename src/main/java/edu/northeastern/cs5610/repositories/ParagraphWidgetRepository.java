ea356848712907d40165173281f5eabdf3b09443
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-01 16:59:03 -0500

    added all t tyoes of widget repositories

---
+++ src/main/java/edu/northeastern/cs5610/repositories/ParagraphWidgetRepository.java
@@ -0,0 +1,9 @@
+package edu.northeastern.cs5610.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import edu.northeastern.cs5610.models.ParagraphWidget;
+
+public interface ParagraphWidgetRepository extends CrudRepository<ParagraphWidget, Integer> {
+
+}
