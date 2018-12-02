491b821047fff612080fed806a03122457da6ee7
hariprasath <hariprasath.sivanandam@gmail.com>
2018-12-01 20:47:09 -0500

    refactored Module Service

--- src/main/java/edu/northeastern/cs5610/services/ModuleService.java
+++ src/main/java/edu/northeastern/cs5610/services/ModuleService.java
@@ -1,6 +1,8 @@
 package edu.northeastern.cs5610.services;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,6 +17,8 @@ import org.springframework.web.bind.annotation.RestController;
 import edu.northeastern.cs5610.models.Course;
 import edu.northeastern.cs5610.models.Module;
 import edu.northeastern.cs5610.models.User;
+import edu.northeastern.cs5610.repositories.CourseRepository;
+import edu.northeastern.cs5610.repositories.ModuleRepository;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -24,32 +28,54 @@ public class ModuleService {
   	UserService userService;
   @Autowired
   	CourseService courseService;
+  @Autowired
+	CourseRepository courseRepository;
+  @Autowired
+	ModuleRepository moduleRepository;
   
     @GetMapping("/api/course/{courseId}/module")
 	public List<Module> findAllModules(@PathVariable("courseId") int courseId) {
-		Course course = courseService.findCourseById(courseId);
-		return course.getModules();
+    	Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			return course.getModules();
+		}
+		return null;
+//		Course course = courseService.findCourseById(courseId);
+//		return course.getModules();
   	}
     
     @PostMapping("/api/course/{cid}/module")
-    public List<Module> createModule(@PathVariable("cid") int courseId, @RequestBody Module module) {
-      Course course = courseService.findCourseById(courseId);
-      course.getModules().add(module);
-      return course.getModules();
+    public Module createModule(@PathVariable("cid") int courseId, @RequestBody Module newModule) {
+    	Optional<Course> data = courseRepository.findById(courseId);
+		if(data.isPresent()) {
+			Course course = data.get();
+			newModule.setCourse(course);
+		    List<Module> new_modules = course.getModules();
+		    new_modules.add(newModule);
+		    course.setModules(new_modules);
+			return moduleRepository.save(newModule);
+		}
+		return null;
     }
     
     @GetMapping("/api/module/{mid}")
     public Module findModuleById(@PathVariable("mid") int moduleId) {
-      List<User> allUsers = userService.findAllUsers();
-      for (User u : allUsers) {
-          List<Course> allCourses = u.getCourses();
-          for (Course c : allCourses) {
-              List<Module> allModules = c.getModules();
-              for (Module m : allModules) 
-                  if (m.getId() == moduleId) 
-                      return m;
-          }
-      }
+    	Optional<Module> data = moduleRepository.findById(moduleId);
+		if(data.isPresent()) {
+			Module module = data.get();
+			return module;
+		}
+//      List<User> allUsers = userService.findAllUsers();
+//      for (User u : allUsers) {
+//          List<Course> allCourses = u.getCourses();
+//          for (Course c : allCourses) {
+//              List<Module> allModules = c.getModules();
+//              for (Module m : allModules) 
+//                  if (m.getId() == moduleId) 
+//                      return m;
+//          }
+//      }
       return null;
     }
     
@@ -63,19 +89,28 @@ public class ModuleService {
     
     @DeleteMapping("/api/module/{mid}")
     public List<Module> deleteModule(@PathVariable("mid") int moduleId) {
-       List<User> allUsers = userService.findAllUsers();
-       for (User u : allUsers) {
-           List<Course> allCourses = u.getCourses();
-           for (Course c : allCourses) {
-              List<Module> allModules = c.getModules();
-              for (Module m : allModules)
-                  if (m.getId() == moduleId) {
-                      allModules.remove(m);
-                      c.setModules(allModules);
-                      return c.getModules();
-                  }
-          }
-      }
+    	Course course  = findModuleById(moduleId).getCourse();
+    	List<Module> modules = course.getModules();
+    	Iterator<Module> iterator= modules.iterator();
+    	while (iterator.hasNext()) {
+    		   Module m = iterator.next();
+    		  if(m.getId()==moduleId)
+    		    iterator.remove();
+    		}
+    	moduleRepository.deleteById(moduleId);
+//       List<User> allUsers = userService.findAllUsers();
+//       for (User u : allUsers) {
+//           List<Course> allCourses = u.getCourses();
+//           for (Course c : allCourses) {
+//              List<Module> allModules = c.getModules();
+//              for (Module m : allModules)
+//                  if (m.getId() == moduleId) {
+//                      allModules.remove(m);
+//                      c.setModules(allModules);
+//                      return c.getModules();
+//                  }
+//          }
+//      }
       return null;
   }	
 }
